97e02c5c47c0edffa1aa59d54c9fc46c161601a9
sammy de la cruz <samy_samantha_5@hotmail.com>
2018-06-23 17:56:14 -0500

    intento de visualizacón de comentarios

--- aplicacion/modulos_administrador/adm_comentarios.php
+++ aplicacion/modulos_administrador/adm_comentarios.php
@@ -149,6 +149,11 @@ if (@!$_SESSION['usuario']) {
             </div>
         </div></br></br></br>
 
+    <!-- begin wwww.htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">HTML Comment Box</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+ <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24M1mHipf844J2rj8CtNSZr%2F"+"&opts=16862&num=10&ts=1529719762354");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end www.htmlcommentbox.com -->
         <footer class="container-fluid text-center">
             <p>Diseño y programación: Elsa Vasco, Edison Tamayo, José Criollo</p>
         </footer>
